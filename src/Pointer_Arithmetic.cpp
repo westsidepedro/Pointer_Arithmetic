71bb29fc0a38fa60578a99efee2fc3aa1e55dd8f
westsidepedro <35743893+westsidepedro@users.noreply.github.com>
2018-01-30 13:09:56 -0600

    Add files via upload

---
+++ src/Pointer_Arithmetic.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Pointer_Arithmetic.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello there Sexy!" << endl; // prints Hello there Sexy!
+	return 0;
+}
